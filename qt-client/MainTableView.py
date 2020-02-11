5f75a74d0eb50b5f883e91f38aaebee3d5bb3fed
Harry Chan <hchan@eyetechds.com>
2020-02-11 13:54:00 -0700

    remove import

--- qt-client/MainTableView.py
+++ qt-client/MainTableView.py
@@ -6,8 +6,6 @@ from pathlib import Path
 from MangaAggreController import MangaAggreController
 from functools import partial
 from ProgressBarDelegate import ProgressBarDelegate
-import numpy as np, os
-from collections import defaultdict
 from Downloader import Downloader
 
 DUMMY_DATA = pd.DataFrame([[1, 2, 3], [2, 4, 6]], columns=['a', 'b', 'c'])
@@ -27,68 +25,6 @@ class MainTableView(QtWidgets.QTableView):
             df['Total Pages'] = 0
             self.setItemDelegateForColumn(
                 df.columns.tolist().index('Progress'), ProgressBarDelegate())
-
-    # def page_download_finished(self, dl_key):
-    #     self.page_downloaded_dict[dl_key] += 1
-    #     total_page = self.total_page_dict[dl_key]
-    #     if self.page_downloaded_dict[dl_key] + 1 == total_page:
-    #         self.page_idx_dict.pop(dl_key)
-    #         self.page_downloaded_dict.pop(dl_key)
-    #         self.total_page_dict.pop(dl_key)
-    #         self.timer_dict.pop(dl_key)
-
-    #         zip_fn = f'{dl_key.as_posix()}.zip'
-    #         zip_path = dl_key.as_posix()
-
-    #         zipdir(zip_fn, zip_path)
-
-
-    # def start_download_work(self, pages, index, dl_key):
-    #     page_idx = self.page_idx_dict[dl_key]
-    #     if page_idx == len(pages):
-    #         self.timer_dict[dl_key].stop()
-    #         return
-    #     output_dir, url = pages[page_idx]
-    #     fn = output_dir/f'{page_idx}'
-    #     worker = Worker(self.ctr.downloadPage, filename=fn,
-    #                     url=url)
-    #     self.page_idx_dict[dl_key] += 1
-    #     self.threadpool.start(worker)
-    #     worker.signals.finished.connect(partial(self.model(
-    #     ).page_download_finished, index))
-    #     worker.signals.finished.connect(partial(self.page_download_finished, dl_key))
-
-        
-
-    # def download_pages(self, index, output):
-    #     output, new_status = output
-    #     self.currentStatus = new_status
-
-    #     pages_to_download = []
-    #     model = self.model()
-    #     total_page_idx = self.df.columns.tolist().index('Total Pages')
-    #     model.setData(model.index(index.row(), total_page_idx), len(output))
-
-    #     for page_d in output:
-    #         name = page_d['name']
-    #         title = page_d['title']
-    #         page = page_d['page']
-    #         url = page_d['url']
-
-    #         output_dir = self.root_path/name/title
-    #         output_dir.mkdir(parents=True, exist_ok=True)
-    #         pages_to_download.append((output_dir, url))
-        
-    #     dl_key = output_dir
-
-    #     self.timer_dict[dl_key] = timer = QtCore.QTimer()
-    #     self.total_page_dict[dl_key] = len(output)
-    #     self.page_idx_dict[dl_key] = 0
-    #     timer.setInterval(50)
-    #     timer.timeout.connect(
-    #         partial(self.start_download_work, pages_to_download, index, dl_key=dl_key))
-
-    #     timer.start()
 
     def search_manga(self):
         keyword = self.parent().textEdit.text()
