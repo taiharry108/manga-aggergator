380d8ab30db9e9996ef1a967606b04858ac82134
Harry Chan <taiharry108@gmail.com>
2020-02-22 19:08:21 -0700

    added MHG

--- qt-client/scraper/MangaSite.py
+++ qt-client/scraper/MangaSite.py
@@ -15,7 +15,7 @@ class MangaSite(QtCore.QObject):
         self._downloader = Downloader()
         self._manga_dict = {}
     
-    def get_manga(self, manga_name, manga_url=None):
+    def get_manga(self, manga_name, manga_url=None) -> Manga:
         if not manga_name in self._manga_dict.keys():
             assert manga_url is not None
             self._manga_dict[manga_name] = Manga(name=manga_name, url=manga_url)
--- qt-client/scraper/MangaSiteFactory.py
+++ qt-client/scraper/MangaSiteFactory.py
@@ -1,10 +1,14 @@
 from enum import Enum
 from ManHuaDui import ManHuaDui
+from ManHuaGui import ManHuaGui
 from MangaSite import MangaSite
 
 class MangaSiteEnum(Enum):
     ManHuaDui = 1
+    ManHuaGui = 2
 
 def get_manga_site(manga_site_enum: MangaSiteEnum) -> MangaSite:
     if manga_site_enum == MangaSiteEnum.ManHuaDui:
         return ManHuaDui()
+    elif manga_site_enum == MangaSiteEnum.ManHuaGui:
+        return ManHuaGui()
