e5cbd7aa28c44590c593d431a494cb585e60d256
Harry Chan <taiharry108@gmail.com>
2020-02-22 19:07:15 -0700

    init checkin

---
+++ qt-client/scraper/ManHuaGui.py
@@ -0,0 +1,155 @@
+from MangaSite import MangaSite
+from bs4 import BeautifulSoup
+from PySide2 import QtNetwork
+from functools import partial
+from Manga import Manga, MangaIndexTypeEnum
+import re
+from Crypto.Cipher import AES
+import base64
+import json
+from urllib import parse
+import string
+import lzstring
+import re
+digs = string.digits + string.ascii_letters
+QNetworkReply = QtNetwork.QNetworkReply
+
+
+def decompress(s):
+    x = lzstring.LZString()
+    return x.decompressFromBase64(s)
+
+
+def int2base(x, base):
+    if x < 0:
+        sign = -1
+    elif x == 0:
+        return digs[0]
+    else:
+        sign = 1
+
+    x *= sign
+    digits = []
+
+    while x:
+        digits.append(digs[int(x % base)])
+        x = int(x / base)
+
+    if sign < 0:
+        digits.append('-')
+
+    digits.reverse()
+
+    return ''.join(digits)
+
+
+def decode(p, a, c, k, d):
+    def e(c):
+        first = "" if c < a else e(int(c/a))
+        c = c % a
+        if c > 35:
+            second = chr(c + 29)
+        else:
+            second = int2base(c, 36)
+        return first + second
+    while c != 0:
+        c -= 1
+        d[e(c)] = k[c] if k[c] != "" else e(c)
+    k = [lambda x: d[x]]
+    def e(): return '\\w+'
+    c = 1
+    while c != 0:
+        c -= 1
+        p = re.sub(f'\\b{e()}\\b', lambda x: k[c](x.group()), p)
+    return p
+
+
+class ManHuaGui(MangaSite):
+    def __init__(self):
+        super(ManHuaGui, self).__init__('漫畫鬼', 'https://www.manhuagui.com/')
+
+    def parse_search_result(self, reply: QNetworkReply, meta_dict: dict):
+        def handle_dict(d):
+            name = d['t']
+            url = d['u']
+            if not url.startswith('http'):
+                url = 'https://www.manhuagui.com/' + url.lstrip('/')
+            manga = self.get_manga(manga_name=name, manga_url=url)
+            return manga
+
+        data = reply.readAll().data()
+        json_data = json.loads(data)
+        result = [handle_dict(d) for d in json_data]
+        self.search_result.emit(result)
+
+    def parse_index_result(self, reply: QNetworkReply, meta_dict: dict):
+        def get_type(idx_type):
+            if idx_type == '单话':
+                type_ = MangaIndexTypeEnum.CHAPTER
+            elif idx_type == '单行本':
+                type_ = MangaIndexTypeEnum.VOLUME
+            else:
+                type_ = MangaIndexTypeEnum.MISC
+            return type_
+
+        data = reply.readAll()
+        soup = BeautifulSoup(data.data(), features="html.parser")
+
+        manga_url = reply.url().toString()
+
+        div = soup.find('div', class_='chapter')
+        idx_types = [h4.find('span').text for h4 in div.find_all('h4')]
+        divs = div.find_all('div', class_='chapter-list')
+        name = soup.find('div', class_='book-title').find('h1').text
+
+        manga = self.get_manga(manga_name=name, manga_url=manga_url)
+
+        for idx_type, div in zip(idx_types, divs):
+            m_type = get_type(idx_type)
+            for ul in div.find_all('ul'):
+                for a in reversed(ul.find_all('a')):
+                    title = a.get('title')
+                    url = a.get('href')
+                    if not url.startswith('http'):
+                        url = 'https://www.manhuagui.com/' + url.lstrip('/')
+                    manga.add_chapter(m_type=m_type, title=title, page_url=url)
+
+        self.index_page.emit(manga)
+
+    def parse_page_urls(self, reply: QNetworkReply, meta_dict: dict):
+        data = reply.readAll()
+        soup = BeautifulSoup(data.data(), features="html.parser")
+
+        pattern = re.compile('window.*return p;}(.*\))\)')
+        for script in soup.find_all('script'):
+            match = pattern.search(script.text)
+            if match:
+                break
+
+        p, a, c, k, e, d = eval(match.group(1).replace(
+            r"['\x73\x70\x6c\x69\x63']('\x7c')", ""))
+        p = decode(p, a, c, decompress(k).split('|'), d)
+
+        match = re.search('SMH.imgData\((.*)\).preInit', p)
+        manga_data = json.loads(match.group(1))
+        cid = manga_data['cid']
+        path = manga_data['path']
+        md5 = manga_data["sl"]["md5"]
+        manga_name = manga_data['bname']
+        chap_title = manga_data['cname']
+        pages = []
+        for idx, file in enumerate(manga_data['files']):
+            page_url = f"https://i.hamreus.com{path}{file}?cid={cid}&md5={md5}"
+            pages.append(page_url)
+        self.get_pages.emit(pages)
+
+    def search_manga(self, keyword):
+        search_url = f'{self.url}tools/word.ashx?key={keyword}'
+        self.downloader.get_request(search_url, self.parse_search_result)
+
+    def get_index_page(self, page):
+        self.downloader.get_request(page, self.parse_index_result)
+
+    def get_page_urls(self, chapter_url):
+        self.downloader.get_request(
+            chapter_url, self.parse_page_urls)
---
+++ qt-client/scraper/testManHuaGui.py
@@ -0,0 +1,129 @@
+import unittest
+import MangaSiteFactory
+from MangaSite import MangaSite
+from Manga import Manga, Chapter, MangaIndexTypeEnum
+from Downloader import Downloader
+from PySide2 import QtWidgets, QtCore
+from typing import List, Dict
+from functools import partial
+
+DLR = Downloader(None)
+
+def call_timeout(loop):
+    loop.quit()
+    raise TimeoutError
+
+
+class TestManHuaGui(unittest.TestCase):
+    def test_init(self):
+        mhg = MangaSiteFactory.get_manga_site(MangaSiteFactory.MangaSiteEnum.ManHuaGui)
+        self.assertEqual(mhg.name, '漫畫鬼')
+        self.assertEqual(mhg.url, 'https://www.manhuagui.com/')
+    
+    
+    def test_seach_manga(self):
+
+        def search_callback(loop, result: List[Manga]):
+            self.assertIsInstance(result, list)
+            self.assertEqual(len(result), 2)
+            foundFlag = False        
+            for manga in result:
+                if manga.name == '火影忍者':
+                    foundFlag = True
+                    break
+            self.assertTrue(foundFlag)
+            self.assertEqual(
+                manga.url, 'https://www.manhuagui.com/comic/4681/')
+            loop.quit()
+
+        timeout = 5000
+        if QtWidgets.QApplication.instance() is None:
+            QtWidgets.QApplication([])
+
+        loop = QtCore.QEventLoop()
+        mhg = MangaSiteFactory.get_manga_site(MangaSiteFactory.MangaSiteEnum.ManHuaGui)
+        
+        mhg.search_result.connect(partial(search_callback, loop))
+        
+        self.assertEqual(DLR, mhg.downloader)
+        mhg.search_manga('naruto')
+        if timeout is not None:
+            QtCore.QTimer.singleShot(timeout, partial(call_timeout, loop))
+        loop.exec_()
+        
+    def test_get_index_page(self):
+        def index_callback(loop, manga: Manga):
+            chapters = manga.chapters
+            self.assertIsInstance(chapters, dict)
+            self.assertEqual(len(chapters), 3)
+            self.assertEqual(len(chapters[MangaIndexTypeEnum.CHAPTER]), 10)
+            chapter = chapters[MangaIndexTypeEnum.VOLUME][-1]
+            self.assertEqual(chapter.title, '第72卷')
+            loop.quit()
+            
+        timeout = 5000
+        if QtWidgets.QApplication.instance() is None:
+            QtWidgets.QApplication([])
+        
+        mhg = MangaSiteFactory.get_manga_site(MangaSiteFactory.MangaSiteEnum.ManHuaGui)
+        mhg.get_index_page('https://www.manhuagui.com/comic/4681/')
+        
+        loop = QtCore.QEventLoop()
+        mhg.index_page.connect(partial(index_callback, loop))
+
+        if timeout is not None:
+            QtCore.QTimer.singleShot(timeout, partial(call_timeout, loop))
+        loop.exec_()
+    
+    def test_get_index_page2(self):
+        def index_callback(loop, manga: Manga):
+            chapters = manga.chapters
+            self.assertIsInstance(chapters, dict)
+            self.assertEqual(len(chapters), 3)
+            self.assertEqual(len(chapters[MangaIndexTypeEnum.CHAPTER]), 195)
+            chapter = chapters[MangaIndexTypeEnum.CHAPTER][156]
+            self.assertEqual(chapter.title, '第157话 归还之魂')
+            loop.quit()
+
+        timeout = 5000
+        if QtWidgets.QApplication.instance() is None:
+            QtWidgets.QApplication([])
+
+        mhg = MangaSiteFactory.get_manga_site(
+            MangaSiteFactory.MangaSiteEnum.ManHuaGui)
+        mhg.get_index_page('https://www.manhuagui.com/comic/19430/')
+
+        loop = QtCore.QEventLoop()
+        mhg.index_page.connect(partial(index_callback, loop))
+
+        if timeout is not None:
+            QtCore.QTimer.singleShot(timeout, partial(call_timeout, loop))
+        loop.exec_()
+    
+    def test_get_page_urls(self):
+        def page_urls_callback(loop, page_urls):
+            self.assertEqual(len(page_urls), 208)
+            self.assertEqual(
+                page_urls[0], 'https://i.hamreus.com/ps1/h/naruto/69/NARUTO69_000.jpg.webp?cid=194193&md5=9rdIwhvMsyOQN8DK0alICQ')
+            self.assertEqual(
+                page_urls[1], 'https://i.hamreus.com/ps1/h/naruto/69/NARUTO69_001b.png.webp?cid=194193&md5=9rdIwhvMsyOQN8DK0alICQ')
+            self.assertEqual(
+                page_urls[-2], 'https://i.hamreus.com/ps1/h/naruto/69/NARUTO69_104.png.webp?cid=194193&md5=9rdIwhvMsyOQN8DK0alICQ')
+            loop.quit()
+
+        timeout = 10000
+        if QtWidgets.QApplication.instance() is None:
+            QtWidgets.QApplication([])
+
+        mhg = MangaSiteFactory.get_manga_site(MangaSiteFactory.MangaSiteEnum.ManHuaGui)
+        mhg.get_page_urls('https://www.manhuagui.com/comic/4681/194193.html')
+        loop = QtCore.QEventLoop()
+
+        mhg.get_pages.connect(partial(page_urls_callback, loop))
+
+
+        if timeout is not None:
+            QtCore.QTimer.singleShot(timeout, partial(call_timeout, loop))
+        loop.exec_()
+
+    
