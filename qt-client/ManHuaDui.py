7922a1a3c715fa65801a93df22f08355c74bf17d
Harry Chan <hchan@eyetechds.com>
2020-02-24 19:19:00 -0700

    encode first

--- qt-client/ManHuaDui.py
+++ qt-client/ManHuaDui.py
@@ -15,7 +15,7 @@ def decrypt(encrypted) -> str:
     encrypted = base64.b64decode(encrypted)
     passphrase = "123456781234567G"
     iv = "ABCDEF1G34123412"
-    aes = AES.new(passphrase, AES.MODE_CBC, iv)
+    aes = AES.new(passphrase.encode('utf-8'), AES.MODE_CBC, iv.encode('utf-8'))
 
     decrypted = aes.decrypt(encrypted)
     return decrypted.strip().decode('utf8')
